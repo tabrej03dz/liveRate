ef3e4d3c5ebe4b270b10459a3496264f192110cc
Kajal kumari <kajaldewan2023@gmail.com>
2025-05-17 17:29:49 +0530

    wtewt

--- resources/views/frontLayouts/main.blade.php
+++ resources/views/frontLayouts/main.blade.php
@@ -6,6 +6,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website Layout</title>
+    <!-- or for .png -->
+<link rel="icon" href="{{asset('asset/images/logo.png')}}" type="image/png">
+
     <link rel="stylesheet" href="{{ asset('asset/css/main-style.css') }}">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
